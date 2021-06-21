547b427b892cb42ba810189a42b147ffd9b2f73a
FengYQ <yiqi@xmind.org>
2021-06-21 15:31:50 +0800

    Add new vc

--- LabelMaker/Model/IconModel.swift
+++ LabelMaker/Model/IconModel.swift
@@ -24,13 +24,16 @@ struct IconImage: Codable {
     private var uuid: String
     
     var image: NSImage {
-        set {
-            newValue.provideImageData(<#T##data: UnsafeMutableRawPointer##UnsafeMutableRawPointer#>, bytesPerRow: <#T##Int#>, origin: <#T##Int#>, <#T##y: Int##Int#>, size: <#T##Int#>, <#T##height: Int##Int#>, userInfo: <#T##Any?#>)
-        }
         get {
             let data = try! Data(contentsOf: docuURL.appendingPathComponent(uuid))
             return NSImage(data: data)!
         }
+    }
+    
+    init(image: NSImage) {
+        //FIXME: Change NSImage to data , write data to file
+        uuid = UUID().uuidString
+        
     }
     
 }
---
+++ LabelMaker/VC/IconEditorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  IconEditorViewController.swift
+//  LabelMaker
+//
+//  Created by Edmund Feng on 2021/6/21.
+//
+
+import AppKit
+
+class IconEditorViewController: NSViewController {
+    
+    
+}
