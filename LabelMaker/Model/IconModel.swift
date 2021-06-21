c35a2cc974675a9d51fdc395fbe3ee75ace439a9
FengYQ <yiqi@xmind.org>
2021-06-21 09:44:43 +0800

    Add model

--- LabelMaker/Model/IconModel.swift
+++ LabelMaker/Model/IconModel.swift
@@ -7,10 +7,30 @@
 
 import AppKit
 
-struct IconModel {
+struct IconModel: Codable {
     
     var uuid: String
     var name: String
-    var image: NSImage
+    var image: IconImage
     var markerStr: String
+}
+
+
+private let docuURL = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+
+//ImagePool
+
+struct IconImage: Codable {
+    private var uuid: String
+    
+    var image: NSImage {
+        set {
+            newValue.provideImageData(<#T##data: UnsafeMutableRawPointer##UnsafeMutableRawPointer#>, bytesPerRow: <#T##Int#>, origin: <#T##Int#>, <#T##y: Int##Int#>, size: <#T##Int#>, <#T##height: Int##Int#>, userInfo: <#T##Any?#>)
+        }
+        get {
+            let data = try! Data(contentsOf: docuURL.appendingPathComponent(uuid))
+            return NSImage(data: data)!
+        }
+    }
+    
 }
---
+++ LabelMaker/Model/IconModelController.swift
@@ -0,0 +1,24 @@
+//
+//  IconModelController.swift
+//  LabelMaker
+//
+//  Created by Edmund Feng on 2021/6/20.
+//
+
+import Foundation
+
+class IconModelController {
+    static var shared = IconModelController()
+    
+    var models: [IconModel] {
+        set {
+            guard let newData = try? JSONEncoder().encode(newValue) else { return }
+            UserDefaults.appGroup.setValue(newData, forKey: "model")
+        }
+        get {
+            let oldData = UserDefaults.appGroup.object(forKey: "model") as? Data ?? Data()
+            guard let oldModels = try? JSONDecoder().decode([IconModel].self, from: oldData) else { return [] }
+            return oldModels
+        }
+    }
+}
